e16f3416759235fd9e8bd939c1ff1c09a823b42e
Pabasara Jayawardhana <pabasarajayawardhana@gmail.com>
2023-04-03 14:31:02 -0400

    Change destination details

--- src/components/Destinations/DestinationDetils.jsx
+++ src/components/Destinations/DestinationDetils.jsx
@@ -4,31 +4,34 @@ import { useLocation } from 'react-router-dom';
 import { animateScroll as scroll } from 'react-scroll';
 import { destinationyList } from '../../Data';
 import './destinations.css'
+import ScrollToTopButton from '../../utill/ScrollToTopButton'
 
 function DestinationDetils() {
 
   const { destinationId } = useParams('destinationId');
 
-  // const navigate = useNavigate();
-
-  // const goBack = () => {
-  //   navigate(-1);
-  // };
-
   const destination = destinationyList.filter(item => item.title.replace(/\s+/g, '-').toLowerCase() === destinationId)[0]
 
   return (
-    <div className="destination-detail-container">
-      {/* <div><button onClick={goBack}>Go Back</button></div> */}
-      <article>
-        <h2>{destination.title}</h2>
-        <img className="left-side" src={destination.imageOne} alt="Any Image" />
-        <p>{destination.paraOne}</p>
-        <img className="right-side" src={destination.imageTwo} alt="Any Image" />
-        <p>{destination.paraTwo}</p>
-        <p>{destination.paraThree}</p>
-      </article>
-    </div>
+    <article>
+      <div className="destination-detail-container">
+        <div className="destination_post_container">
+          <h2>{destination.title}</h2>
+          <div className="post">
+            <p>{destination.paraOne}</p>
+            <p >{destination.paraTwo}</p>
+            <p >{destination.paraThree}</p>
+            <div className='destination_image_container'>
+            <figure><img src={destination.imageOne} alt="Any Image" /> </figure>
+            <figure><img src={destination.imageTwo} alt="Any Image" /> </figure>
+            </div>
+           
+          </div>
+
+        </div>
+      </div>
+      <ScrollToTopButton />
+    </article>
   )
 }
 
