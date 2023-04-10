33bf8f55e7f710bc43603760d934a02adda13556
Pabasara Jayawardhana <pabasarajayawardhana@gmail.com>
2023-04-10 10:05:26 -0400

    Add footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
     <div className="footer-container">
 <footer>
 <div className="image-row">
-  <img src={So} alt="Image 1"/>
-  <img src={STDA} alt="Image 2"/>
-  <img src={STPB} alt="Image 3"/>
+  <a href='https://www.srilanka.travel/' target='_blank'><img src={So} alt="Image 1"/></a>
+  <a href='https://www.sltda.gov.lk/en' target='_blank'><img src={STDA} alt="Image 2"/></a>
+  <a href='https://meetinsrilanka.com/logo/sri-lanka-tourism-promotion-bureau-sltpb/' target='_blank'><img src={STPB} alt="Image 3"/></a>
 </div>
   <div className="ft-main">
     <div className="ft-main-item">
@@ -46,7 +46,7 @@ function Footer() {
   <div className="ft-social">
     <ul className="ft-social-list">
       <li><a  href='https://www.facebook.com/people/Ceylon-Beyond-Sky-Tours/100089761681729/?mibextid=LQQJ4d' target='_blank'><SiFacebook/></a></li>
-      <li><a href="#"><SiInstagram/></a></li>
+      {/* <li><a href="#"><SiInstagram/></a></li> */}
       <li><a href="https://en.tripadvisor.com.hk/Attraction_Review-g668534-d25267725-Reviews-Ceylon_Beyond_sky_Tours-Kurunegala_North_Western_Province.html" target='_blank'><SiTripadvisor/></a></li>
       <li><a href="tel:+94 71 998 8108"><MdWifiCalling1/></a></li>
       <li><a href="https://wa.me/+94713960988"><SiWhatsapp/></a></li>
